2ade63ba63a6e7cef98721b0f682d80081e3040f
Camila Rivera <camila.riverart@gmail.com>
2019-10-03 00:14:35 -0700

    Editing Appointment done

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -14,6 +14,7 @@ const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
 
 export default function Appointment({ id, interview, time, interviewers, bookInterview, cancelInterview }) {
 
@@ -36,8 +37,6 @@ export default function Appointment({ id, interview, time, interviewers, bookInt
 
   }
 
-
-
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
   );
@@ -48,12 +47,15 @@ export default function Appointment({ id, interview, time, interviewers, bookInt
         <Header time={time}></Header>
         {mode === EMPTY && <Empty onAdd={(event) => { transition(CREATE) }} />}
         {mode === SHOW && (
-          <Show onDelete = {(event) => {transition(CONFIRM)}}
+          <Show 
+            onDelete = {(event) => {transition(CONFIRM)}}
             student={interview.student}
             interviewer={interview.interviewer}
+            onEdit = {(event) => { transition(EDIT) }}
           />
         )}
         {mode === CREATE && <Form onSave={save} interviewers={interviewers} onCancel={(event) => { back(EMPTY) }} />}
+        {mode === EDIT && <Form onSave={save} name={interview.student} interviewer = {interview.interviewer.id} interviewers={interviewers} onCancel={(event) => { back(SHOW) }} />}
         {mode === SAVING && <Status message = {"Saving"}/>}
         {mode === DELETING && <Status message = {"Deleting"}/>}
         {mode === CONFIRM && <Confirm onConfirm = {deleteInterview} onCancel={(event) => { transition(SHOW) }} message = {"Are you sure you would like to delete this?"}/>}
