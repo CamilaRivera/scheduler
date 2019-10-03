d232994b8809c0250bfa5ad1763a8f5a5014bbdf
Camila Rivera <camila.riverart@gmail.com>
2019-10-02 23:41:57 -0700

    Deleting Appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -16,7 +16,7 @@ export default function Application(props) {
 
   const setDay = day => setState(prev => ({ ...prev, day }));
 
-  function bookInterview(id, interview) {
+  function bookInterview(id, interview){
     console.log("BookInterview", id, interview);
     const appointment = {
       ...state.appointments[id],
@@ -32,6 +32,17 @@ export default function Application(props) {
         setState(prev => ({...prev, appointments }));
       });
     
+  };
+
+  function cancelInterview(id){
+    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
+      .then(response => {
+        setState(prev => {
+          const newState = JSON.parse(JSON.stringify(prev));
+          newState.appointments[id].interview = null;
+          return newState;
+        });
+      });
   }
 
   useEffect(() => {
@@ -43,7 +54,7 @@ export default function Application(props) {
       console.log(all[0].data, all[1].data, all[2].data)
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
     })
-  }, [])
+  }, []);
 
   const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
@@ -59,6 +70,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={interviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,16 +4,18 @@ import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
 import Status from "./Status";
-import { useVisualMode } from "../../hooks/useVisualMode"
 import Form from "./Form";
+import Confirm from "./Confirm";
+import { useVisualMode } from "../../hooks/useVisualMode"
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
-export default function Appointment({ id, interview, time, interviewers, bookInterview }) {
-
+export default function Appointment({ id, interview, time, interviewers, bookInterview, cancelInterview }) {
 
   function save(name, interviewer) {
     const interview = {
@@ -26,6 +28,16 @@ export default function Appointment({ id, interview, time, interviewers, bookInt
 
   }
 
+  function deleteInterview() {
+    transition(CONFIRM);
+    transition(DELETING);
+    cancelInterview(id)
+      .then(() => transition(EMPTY));
+
+  }
+
+
+
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
   );
@@ -36,13 +48,15 @@ export default function Appointment({ id, interview, time, interviewers, bookInt
         <Header time={time}></Header>
         {mode === EMPTY && <Empty onAdd={(event) => { transition(CREATE) }} />}
         {mode === SHOW && (
-          <Show
+          <Show onDelete = {(event) => {transition(CONFIRM)}}
             student={interview.student}
             interviewer={interview.interviewer}
           />
         )}
         {mode === CREATE && <Form onSave={save} interviewers={interviewers} onCancel={(event) => { back(EMPTY) }} />}
-        {mode === SAVING && <Status message = {"Loading"}/>}
+        {mode === SAVING && <Status message = {"Saving"}/>}
+        {mode === DELETING && <Status message = {"Deleting"}/>}
+        {mode === CONFIRM && <Confirm onConfirm = {deleteInterview} onCancel={(event) => { transition(SHOW) }} message = {"Are you sure you would like to delete this?"}/>}
       </Fragment>
 
     </article>
