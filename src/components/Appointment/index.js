e6e8e6cf6b8e50a5f94d41f5770b3b9918d0b38d
Camila Rivera <camila.riverart@gmail.com>
2019-10-02 17:56:52 -0700

    Added useVisualMode function to Appointment components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,35 @@ import "./styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
+import { useVisualMode } from "../../hooks/useVisualMode"
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment({ id, interview, time }) {
+
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+
   return (
-    <article className = "appointment">
-    <Fragment>
-      <Header time={time}></Header>
-      {interview && <Show interviewer={interview.interviewer} student={interview.student}></Show>}
-      {!interview && <Empty></Empty>}
-    </Fragment>
+    <article className="appointment">
+      <Fragment>
+        <Header time={time}></Header>
+        {mode === EMPTY && <Empty onAdd={(event) => {transition(CREATE)}} />}
+        {mode === SHOW && (
+          <Show
+            student={interview.student}
+            interviewer={interview.interviewer}
+          />
+        )}
+        { mode === CREATE && <Form interviewers={[]} onCancel={(event) => {back(EMPTY)}}/> }
+      </Fragment>
+
     </article>
   )
 }
+
+
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  function transition(newMode, replace = false) {
+    if(replace){
+      history.push(newMode);
+    }
+    setMode(newMode);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop(history[history.length - 1]);
+    }
+    setMode(history[history.length-1]);
+  }
+
+  return { mode, transition, back };
+}
